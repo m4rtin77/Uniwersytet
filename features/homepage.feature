cc44d7313a7a1c379a0332f71caa051784578a91
m4rtin77 <m4rtin77@student.kul.lublin.pl>
2015-01-23 09:27:39 +0100

    Homepage - GREEN | Michal Martyna

--- features/homepage.feature
+++ features/homepage.feature
@@ -3,4 +3,4 @@ Feature: Web pages
   Scenario: Homepage
     Given I am on homepage
      Then the response status code should be 200
-     Then I should see "This is the University homepage"
+     Then I should see "This is the University Symfony2 APP."
--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -1,14 +1,13 @@
 <?php
 
 namespace AppBundle\Controller;
-
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 
 class DefaultController extends Controller
 {
     /**
-     * @Route("/app/example", name="homepage")
+     * @Route("/", name="homepage")
      */
     public function indexAction()
     {
